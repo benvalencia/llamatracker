e6dca3d0d06257f6d2d0a5382ec0003630ca2379
Ben <benyamin.ak@hotmail.com>
2024-05-13 19:51:14 +0200

    feature(miki): bable config y gitignore

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
- const { Client, Language } = require('fnapicom');
-
- const client = new Client({
-  language: Language.English,
-  apiKey: '2eb358b4-ef78-41ce-aecb-961725393619',
-});
-
- client.aesKeys()
-  .then(console.log);
+module.exports = function (api) {
+ api.cache(true);
+ return {
+  presets: ['babel-preset-expo'],
+ };
+};
