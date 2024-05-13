22408c14b6f52bdffe2588b7abf210fc616cc26c
Ben <benyamin.ak@hotmail.com>
2024-05-13 19:22:36 +0200

    feature(13): arquitectura del Llama tracker

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,13 @@ import {createStackNavigator} from '@react-navigation/stack';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import HomeScreen from "@/app/screens/home/home";
+import ValidateAccountScreen from "@/app/screens/site/validateAccount";
+import NotFoundScreen from "@/app/screens/site/notFound";
+import ForgotPasswordScreen from "@/app/screens/auth/forgotPassword";
+import RegisterScreen from "@/app/screens/auth/register";
+import LoginScreen from "@/app/screens/auth/login";
+import ProfileScreen from "@/app/screens/profile/profile";
+import StatsScreen from "@/app/screens/profile/stats";
 
 const Stack = createStackNavigator();
 
@@ -34,6 +41,29 @@ export default function RootLayout() {
       <Stack.Navigator initialRouteName="screens/home/home">
         <Stack.Screen name="screens/home/home" options={{title: 'Home', headerShown: false, gestureEnabled: false}}
                       component={HomeScreen}/>
+
+        {/*PROFILE*/}
+        <Stack.Screen name="screens/profile/stats" options={{title: 'Stats', headerShown: false, gestureEnabled: false}}
+                      component={StatsScreen}/>
+        <Stack.Screen name="screens/profile/profile"
+                      options={{title: 'Profile', headerShown: false, gestureEnabled: false}}
+                      component={ProfileScreen}/>
+        {/*AUTH*/}
+        <Stack.Screen name="screens/auth/login" options={{title: 'Login', headerShown: false, gestureEnabled: false}}
+                      component={LoginScreen}/>
+        <Stack.Screen name="screens/auth/register"
+                      options={{title: 'Register', headerShown: false, gestureEnabled: false}}
+                      component={RegisterScreen}/>
+        <Stack.Screen name="screens/auth/forgotPassword"
+                      options={{title: 'Forgot password', headerShown: false, gestureEnabled: false}}
+                      component={ForgotPasswordScreen}/>
+        {/*SITE*/}
+        <Stack.Screen name="screens/site/notFound"
+                      options={{title: 'Not found', headerShown: false, gestureEnabled: false}}
+                      component={NotFoundScreen}/>
+        <Stack.Screen name="screens/site/validateAccount"
+                      options={{title: 'Validate account', headerShown: false, gestureEnabled: false}}
+                      component={ValidateAccountScreen}/>
       </Stack.Navigator>
     </ThemeProvider>
   );
---
+++ app/screens/profile/profile.tsx
@@ -0,0 +1,40 @@
+import {StyleSheet, View, Text, TextInput, Button} from 'react-native';
+import {Colors} from "@/constants/Colors";
+
+export default function ProfileScreen() {
+  return (
+    <View style={{
+      backgroundColor: Colors.primary,
+      height: '100%',
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <Text>
+        imagen
+      </Text>
+      <TextInput placeholder={'Buscar'}>
+      </TextInput>
+      <Button title={'Buscar'}></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
