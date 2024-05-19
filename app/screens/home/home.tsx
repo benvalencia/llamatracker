c1fc378a8efe857c5860ff92f6d8812de88a8a37
MMARTID <miguelmartindelgado1998@gmail.com>
2024-05-19 03:50:55 +0200

    feature(29): days a temporizador

--- app/screens/home/home.tsx
+++ app/screens/home/home.tsx
@@ -38,10 +38,11 @@ export default function HomeScreen() {
       const difference = nextUpdate.getTime() - now.getTime();
 
       if (difference > 0) {
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        setTimeRemaining(`${hours}h ${minutes}m ${seconds}s`);
+        setTimeRemaining(`${days}D ${hours}h ${minutes}m ${seconds}s`);
       } else {
         setTimeRemaining('¡La actualización ha llegado!');
       }
