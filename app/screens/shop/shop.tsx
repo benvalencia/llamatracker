b2b48fd3e584087e655cbfefd2df20f8071bcfa5
Ben <benyamin.ak@hotmail.com>
2024-05-21 05:01:11 +0200

    feature(56): image resolver

--- app/screens/shop/shop.tsx
+++ app/screens/shop/shop.tsx
@@ -4,10 +4,12 @@ import {FortniteService} from "@/app/services/fortnite/fortnite.service";
 import {useNavigation} from "expo-router";
 import React, {useEffect, useState} from "react";
 import {CommonActions} from "@react-navigation/native";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 export default function ShopScreen() {
   const fortniteService = new FortniteService();
   const navigation = useNavigation()
+  const {top} = useSafeAreaInsets()
 
   const [shopList, setShopList] = useState({} as any);
   const [refreshing, setRefreshing] = useState(false);
@@ -25,9 +27,7 @@ export default function ShopScreen() {
   const getShopList = async () => {
     const getShop = await fortniteService.getBatelRoyaleShop();
 
-    // console.log('banner >>>', getShop.data.featured.entries[0].banner)
-    // console.log('items >>>', getShop.data.featured.entries[0].items)
-    console.log('newDisplayAsset.materialInstances >>>', getShop.data.featured.entries[0].newDisplayAsset.materialInstances)
+    console.log('items >>>', getShop.data.featured.entries[1].bundle.image)
 
     setShopList(getShop.data);
 
@@ -62,7 +62,7 @@ export default function ShopScreen() {
 
   return (
     <ScrollView
-      contentContainerStyle={styles.container}
+      contentContainerStyle={[styles.container, {paddingTop: top}]}
       refreshControl={
         <RefreshControl refreshing={refreshing}
                         onRefresh={onRefresh}
@@ -75,10 +75,17 @@ export default function ShopScreen() {
         justifyContent: 'center'
       }}>
         <View>
-          <Text>daily: {shopList.daily}</Text>
-          <Text>date: {shopList.date}</Text>
+          {/*<Text>daily: {shopList.daily}</Text>*/}
+          <Text>{new Date(shopList.date).toLocaleDateString('spanish', {
+            weekday: "long",
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+          })}</Text>
           <Text>hash: {shopList.hash}</Text>
-          <Text>vbuckIcon: {shopList.vbuckIcon}</Text>
+          {/*<Text>vbuckIcon: {shopList.vbuckIcon}</Text>*/}
+          <Image source={{uri: shopList.vbuckIcon}} width={45} height={45}></Image>
+
         </View>
         <View style={styles.shopListContainer}>
           {/*<NewsItemBuilder></NewsItemBuilder>*/}
@@ -87,45 +94,63 @@ export default function ShopScreen() {
             return (
               <Pressable style={styles.itemContainer} key={index} onPress={() => goToItemDetail(item)}>
                 <Text>categories: {item.categories}</Text>
+                <Text>finalPrice: {item.finalPrice}</Text>
+                <Text>regularPrice: {item.regularPrice}</Text>
+                <Text>section: {item.section}</Text>
+
                 {/*<Text>devName: {item.devName}</Text>*/}
                 {/*<Text>displayAssetPath: {item.displayAssetPath}</Text>*/}
-                <Text>finalPrice: {item.finalPrice}</Text>
                 {/*<Text>giftable: {item.giftable ? 'true' : 'false'}</Text>*/}
                 {/*<Text>newDisplayAssetPath: {item.newDisplayAssetPath}</Text>*/}
                 {/*<Text>offerId: {item.offerId}</Text>*/}
                 {/*<Text>refundable: {item.refundable ? 'true' : 'false'}</Text>*/}
-                <Text>regularPrice: {item.regularPrice}</Text>
-                <Text>section: {item.section}</Text>
-
-
                 {/*<Text>sectionId: {item.sectionId}</Text>*/}
                 {/*<Text>sortPriority: {item.sortPriority}</Text>*/}
                 {/*<Text>tileSize: {item.tileSize}</Text>*/}
 
+                {/*banner INFORMATION*/}
+                {/*<Text>banner.backendValue: {item.banner?.backendValue}</Text>*/}
+                {/*<Text>banner.intensity: {item.banner?.intensity}</Text>*/}
+                {/*<Text>banner.value: {item.banner?.value}</Text>*/}
+
                 {/*NewDisplayAsset INFORMATION*/}
-                {/*<Text>newDisplayAsset.cosmeticId: {item.newDisplayAsset?.cosmeticId}</Text>*/}
-                <Text>newDisplayAsset.id: {item.newDisplayAsset?.id}</Text>
-                {/*<Text>newDisplayAsset.materialInstances: {item.newDisplayAsset?.materialInstances[0]}</Text>*/}
-                {/*<Text>newDisplayAsset.cosmeticId: {item.newDisplayAsset?.cosmeticId}</Text>*/}
-                {/*<Text>newDisplayAsset.cosmeticId: {item.newDisplayAsset?.cosmeticId}</Text>*/}
+                {/*<Text>newDisplayAsset.id: {item.newDisplayAsset?.id}</Text>*/}
 
 
                 {/*LAYOUT INFORMATION*/}
-                <Image source={{uri: item.layout?.background}} width={100} height={100}></Image>
-                <Text>layout.category: {item.layout?.category}</Text>
-                <Text>layout.id: {item.layout?.id}</Text>
-                <Text>layout.index: {item.layout?.index}</Text>
+                {/*<Text>layout.background: {item.layout?.background}</Text>*/}
+                {/*<Image source={{uri: item.layout?.background}} style={{*/}
+                {/*  height: 150,*/}
+                {/*  objectFit: 'contain'*/}
+                {/*}}></Image>*/}
+                {/*<Text>layout.category: {item.layout?.category}</Text>*/}
+                {/*<Text>layout.id: {item.layout?.id}</Text>*/}
+                {/*<Text>layout.index: {item.layout?.index}</Text>*/}
                 <Text>layout.name: {item.layout?.name}</Text>
                 <Text>layout.showIneligibleOffers: {item.layout?.showIneligibleOffers}</Text>
 
                 {/*BUNDLE INFORMATION*/}
                 {item.bundle?.info === 'Bundle' ?
                   <View>
-                    <Image source={{uri: item.bundle?.image}} width={100} height={100}/>
+                    <View>
+                      {/*<Text>bundle.image: {item.bundle?.image}</Text>*/}
+                      <Image source={{uri: item.bundle?.image}} style={{
+                        width: 150,
+                        height: 150,
+                        zIndex: 1,
+                        objectFit: 'contain',
+                        position: 'absolute',
+                        alignSelf: 'center',
+                      }}/>
+                      <Image source={{uri: item.layout?.background}} style={{
+                        height: 150,
+                      }}></Image>
+                    </View>
                     {/*<Text>bundle.info: {item.bundle?.info}</Text>*/}
                     <Text>bundle.name: {item.bundle?.name}</Text>
                   </View> : null
                 }
+
               </Pressable>
             )
           })}
