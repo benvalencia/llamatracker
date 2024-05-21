66bf7b16380c9918e35187805c4899a4ab74416a
MMARTID <miguelmartindelgado1998@gmail.com>
2024-05-21 06:49:09 +0200

    f70 0.1

--- app/screens/news/news.tsx
+++ app/screens/news/news.tsx
@@ -18,10 +18,9 @@ export default function NewsScreen() {
     navigation.dispatch(
       CommonActions.navigate({
         name: 'screens/news/newsDetail',
-        params: {
-          newsDetail
-        }
-      }));
+        params: {newsDetail}
+      })
+    );
   }
 
   const getNewsList = async () => {
@@ -57,20 +56,25 @@ export default function NewsScreen() {
         height: '100%',
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
-      }}>
-        {/*NEWS LIST*/}
-        <View style={styles.newsListContainer}>
-          {newsBattleRoyaleList.map((news, index: number) => {
+        justifyContent: 'center'}}
+      > {/*NEWS LIST*/}
+        <View style={styles.newsContainer}>
+          <Text style={styles.sectionTitle}>Battle Royale News</Text>
+               {newsBattleRoyaleList.map((news, index: number) => {
             return (
               <Pressable style={styles.newsItemContainer} key={index} onPress={() => goToNewsDetail(news)}>
-                <Text>body: {news.body}</Text>
-                <Text>hidden: {news.hidden ? 'true' : 'false'}</Text>
-                <Text>id: {news.id}</Text>
-                <Text>image: {news.image}</Text>
-                <Image
-                  source={{uri: news.image}}
-                  style={{width: 325, height: 185}}/>
+                <View style={styles.titleContainer}>
+                   <Text style={styles.sectionTitle}>{news.body}</Text>
+                </View>  
+                <View style={styles.newsImage}>
+                 <Image style={styles.newsImage}
+                   source={{uri: news.image}}/>
+                </View>  
+                    <View style={styles.textContainer}>
+                      <Text style={styles.sectionTitle}>texto</Text>
+
+                    </View>
+                
                 <Text>sortingPriority: {news.sortingPriority}</Text>
                 <Text>tabTitle: {news.tabTitle}</Text>
                 <Text>titleImage: {news.titleImage}</Text>
@@ -86,7 +90,7 @@ export default function NewsScreen() {
                 <Text>image: {news.image}</Text>
                 <Image
                   source={{uri: news.image}}
-                  style={{width: 325, height: 185}}/>
+                  style={{width: 640, height: 305}}/>
                 <Text>title: {news.title}</Text>
               </Pressable>
             )
@@ -106,7 +110,44 @@ const styles = StyleSheet.create({
   scrollReloadContainer: {
     backgroundColor: Colors.primary,
   },
-
+  newsContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 10,
+  },
+  
+  sectionTitle: {
+    paddingTop: 1,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.secondary,
+    marginBottom: 10,
+  },
+  newsItem: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  newsImage: {
+    alignContent:'center',
+    marginRight: 10,
+    justifyContent: 'center',
+    width: 410, 
+    height: 260,
+    alignSelf: 'center', // Centrar horizontalmente
+    resizeMode: 'cover'
+  },
+  newsTitle: {
+    fontSize: 16,
+    color: Colors.yellow,
+  },
+  
   // NEWS LIST CONTAINER
   newsListContainer: {
     backgroundColor: 'red',
@@ -119,5 +160,18 @@ const styles = StyleSheet.create({
   },
   newsItemComponent: {
     backgroundColor: 'pink',
+  },
+  titleContainer: {
+    width: '100%',
+    backgroundColor: Colors.tertiary,
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    alignItems: 'center',
   }
 });
